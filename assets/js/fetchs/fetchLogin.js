d0f4be8c80dc7c5aa0b2b224d69065be9ce336e6
Masud03Am <masudboy2203@gmail.com>
2024-05-24 15:52:07 +0500

    New-Changes

--- assets/js/fetchs/fetchLogin.js
+++ assets/js/fetchs/fetchLogin.js
@@ -18,7 +18,9 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     fetch('http://185.121.2.208/hi-usa/public/auth/login', options)
         .then(response => {
             if (!response.ok) {
-                throw new Error('Network response was not ok: ' + response.statusText);
+                return response.json().then(errorData => {
+                    throw new Error(`Network response was not ok: ${response.status} ${response.statusText} - ${errorData.message}`);
+                });
             }
             return response.json();
         })
